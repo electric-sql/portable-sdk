44aba776ecdfb9b5bbd5f6a6d5db3e726a99f509
pmp-p <mail.peny@free.fr>
2025-04-28 13:42:23 +0200

    sync sdk to .8

--- python-wasm-sdk/python-wasm-sdk.sh
+++ python-wasm-sdk/python-wasm-sdk.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 reset
 
+if [ $UID -ne 0 ]; then
+    echo "not UID 0, assuming not docker"
+else
+    echo "UID 0, assuming docker debian:stable"
+    apt-get update && apt-get --yes install build-essential clang autoconf wget curl lz4 lsb-release zlib1g-dev libssl-dev git
+fi
+
 [ -f ../config ] && . ../config
 
 # TODO: check how dbg tools work with default settings
@@ -21,11 +28,19 @@ then
     . /etc/lsb-release
     export PLATFORM=linux
 else
-    # or not
-    export DISTRIB_ID=$($SYS_PYTHON -E -c "print(__import__('sysconfig').get_config_var('HOST_GNU_TYPE'))")
-    export PLATFORM=$($SYS_PYTHON -E -c "print(__import__('sys').platform)")
-    echo no /etc/lsb-release found, please identify platform $PLATFORM : \"${DISTRIB_ID}-${DISTRIB_RELEASE}\" or hit enter to continue
-    read
+    # is it Debian
+    if [ -f /etc/os-release ]
+    then
+        . /etc/os-release
+        export DISTRIB_ID="${ID}${VERSION_ID}"
+        export DISTRIB_RELEASE=$(arch)
+    else
+        # or not
+        export DISTRIB_ID=$($SYS_PYTHON -E -c "print(__import__('sysconfig').get_config_var('HOST_GNU_TYPE'))")
+        export PLATFORM=$($SYS_PYTHON -E -c "print(__import__('sys').platform)")
+        echo no /etc/lsb-release found, please identify platform $PLATFORM : \"${DISTRIB_ID}-${DISTRIB_RELEASE}\" or hit enter to continue
+        read
+    fi
 fi
 
 export DISTRIB="${DISTRIB_ID}-${DISTRIB_RELEASE}"
@@ -78,6 +93,8 @@ BUILDS=${BUILDS:-"3.12 3.13"}
 for PYBUILD in $BUILDS
 do
     cd "$ORIGIN"
+
+    export PYBUILD
 
 
     if [ -f ${SDKROOT}/dev ]
@@ -334,13 +351,13 @@ END
             ${SDKROOT}/lang/nimsdk.sh
         fi
 
-        mkdir -p /tmp/sdk
+        mkdir -p /tmp/sdk/dist
         # pack extra build scripts
         pushd /
             tar -cpPRz \
              ${SDKROOT}/scripts/emsdk-extra.sh \
              ${SDKROOT}/scripts/emsdk-fetch.sh \
-             ${SDKROOT}/sources.extra/* > /tmp/sdk/sdk-extra.tar.gz
+             ${SDKROOT}/sources.extra/* > /tmp/sdk/dist/sdk-extra.tar.gz
 
             # pack sdl as minimal prebuilt tar, and use lz4 compression on it
             . ${SDKROOT}/scripts/pack-sdk.sh
